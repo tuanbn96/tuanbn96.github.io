61420e699b03512b2e23985af8b8f0fe73716d79
tuanbn96 <quankun7@hotmail.com>
2019-07-04 02:07:13 +0700

    ff

---
+++ kiem_tra_js/DOM_exercise1.js
@@ -0,0 +1,5 @@
+let logo = document.getElementById("hplogo");
+logo.src = 'https://cdn.pixabay.com/photo/2013/01/29/22/53/yahoo-76684_960_720.png';
+logo.srcset = '';
+let search = document.getElementsByClassName('gNO89b')[1];
+search.value = 'Yahoo!';
