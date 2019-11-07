6d314af0d70dc3212e4c68063037158cf95ba67c
Nguyễn Văn Tuân <quankun7@hotmail.com>
2019-11-07 15:04:27 +0700

    ...

---
+++ react-project2/precache-manifest.aca621d18fb702a03b7b9570e5c32f85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e772c8de038c96aac026af3661e4d0",
+    "url": "/react-project2/index.html"
+  },
+  {
+    "revision": "7c6b9a67930191fb75f3",
+    "url": "/react-project2/static/css/main.1000825c.chunk.css"
+  },
+  {
+    "revision": "ccdaffe0cbcf2ace8637",
+    "url": "/react-project2/static/js/2.f0b9c3ac.chunk.js"
+  },
+  {
+    "revision": "7c6b9a67930191fb75f3",
+    "url": "/react-project2/static/js/main.2c714818.chunk.js"
+  },
+  {
+    "revision": "6e47ec19cdbf181288d8",
+    "url": "/react-project2/static/js/runtime-main.377542ca.js"
+  },
+  {
+    "revision": "5bcf25fda6281fab8b076d6afc90a0bc",
+    "url": "/react-project2/static/media/logo.5bcf25fd.png"
+  }
+]);
